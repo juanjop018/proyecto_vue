b84c33f96a6466ddef803617a7c3d30715c6e04b
juanjop018 <jpenaquinonez@gmail.com>
2024-12-09 23:25:05 -0500

    LOGIN Y CALENDARIO

---
+++ src/views/AdminViews/ClassView.vue
@@ -0,0 +1,59 @@
+<template>
+    <body>
+        <header>
+            <NavBar />
+        </header>
+        <div class="container">
+            <h1>Programar Clases</h1>
+            <div class="container-table">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Profesor</th>
+                            <th>Sede</th>
+                            <th>Fecha</th>
+                            <th>Hora</th>
+                            <th>Clase</th>
+                            <th>Dificultad</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Miguel Angel Ayala Roncancio</td>
+                            <td>Bogota</td>
+                            <td>22/04/2024</td>
+                            <td>12:30</td>
+                            <td>Bachata</td>
+                            <td>Dificil</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="buttons">
+                <button>Programar</button>
+                <button>Eliminar</button>
+                <button>Editar</button>
+                <button>Información</button>
+            </div>
+        </div>
+        <footer>
+            <FooterBar /> 
+        </footer>   
+    </body>
+</template>
+
+<script>
+import NavBar from '../../components/NavBar.vue';
+import FooterBar from '../../components/FooterBar.vue'
+
+export default {
+    components: {
+        NavBar,
+        FooterBar,
+    },              
+};  
+</script>
+
+<style scoped>
+@import '../../assets/styles/ClassView';
+</style> 
---
+++ src/views/AdminViews/vadminc.vue
@@ -0,0 +1,46 @@
+<template>
+    <body>
+        <nav class="navbar navbar-expand-lg fixed-top nb">
+        <div class="container-fluid">
+            <a class="navbar-brand me-auto" href="Connection.html">
+            <img class="imgt" src="@/assets/icons/logo.png" alt="Logo" />
+            </a>
+            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="offcanvasNavbarLabel">
+                <img src="@/assets/icons/logo.png" alt="Logo" />
+                </h5>
+                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+                <ul class="navbar-nav justify-content-center flex-grow-1 pe-3">
+                <li class="nav-item" v-for="(link, index) in navLinks" :key="index">
+                    <a class="nav-link mx-lg-2" :href="link.href">{{ link.name }}</a>
+                </li>
+                </ul>
+            </div>
+            </div>
+            <a href="Perfil.html" class="icon iconp">
+            <ion-icon name="person-circle-outline"></ion-icon>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+        </nav>
+    </body>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const navLinks = ref([
+  { name: 'Home', href: 'Connection.html' },
+  { name: 'Clases', href: 'ClassProgramingAdmin.html' },
+  { name: 'Eventos', href: 'Events.html' },
+  { name: 'Reportes', href: 'Report.html' },
+  { name: 'Notificaciones', href: 'Notificacion.html' },
+  { name: 'Administración de Roles', href: 'StudentAdmission.html' },
+  
+]);
+</script>
